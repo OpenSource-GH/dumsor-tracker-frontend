bad6dace37962fb1c3522d1ac17c0985c384608e
lasagna <arthurebenezer@aol.com>
2024-04-24 11:22:37 +0000

    UI for Create Log Page

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,19 +1,32 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "default" | "large"; // Added variants so we can use the same component but with different styles
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, variant = "default", ...props }, ref) => {
+    const baseClassName =
+      "flex bg-transparent py-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50";
+    let specificClassName = "";
+
+    switch (variant) {
+      case "large":
+        specificClassName = "min-h-[60px] w-3/4 text-2xl placeholder:text-2xl";
+        break;
+      case "default":
+      default:
+        specificClassName =
+          "h-9 w-full rounded-md border border-input px-3 py-1 text-sm shadow-sm transition-colors focus-visible:ring-1 focus-visible:ring-ring";
+        break;
+    }
+
     return (
       <input
         type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
+        className={cn(baseClassName, specificClassName, className)}
         ref={ref}
         {...props}
       />
