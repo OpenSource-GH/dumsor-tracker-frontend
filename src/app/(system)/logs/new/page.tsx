0625fe9a8d02973346ffe5d65bec8b03b491d969
Elvis020 <spaceyel3@gmail.com>
2024-05-05 20:07:45 +0000

    feat: redirect unauthorized users upon creating new logs

--- src/app/(system)/logs/new/page.tsx
+++ src/app/(system)/logs/new/page.tsx
@@ -1,9 +1,17 @@
 import CreateLogForm from "./components/create-log";
+import {redirect} from "next/navigation";
+import {createClient} from "@/utils/supabase/server";
 
 async function CreateLogPage() {
+  const supabase = await createClient();
+
+  const {data, error} = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/sign-in");
+  }
   return (
     <div className="max-w-lg mx-auto px-6">
-      <CreateLogForm />
+      <CreateLogForm/>
     </div>
   );
 }
