4add5c2043a4a1273d8a69f2bb4c3a823772a56f
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-05-09 18:18:48 +0000

    fix: linting

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -16,12 +16,16 @@ export async function createClient() {
         set(name: string, value: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value, ...options });
-          } catch (error) {}
+          } catch (error) {
+            console.log(error);
+          }
         },
         remove(name: string, options: CookieOptions) {
           try {
             cookieStore.set({ name, value: "", ...options });
-          } catch (error) {}
+          } catch (error) {
+            console.log(error);
+          }
         },
       },
     }
