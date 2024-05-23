502a2f83050cfa0375cba62f4a5ad3acd766fe8d
lasagna <arthurebenezer@aol.com>
2024-05-23 09:04:05 +0000

    feat: show time on and off

--- src/components/cards/log-card.tsx
+++ src/components/cards/log-card.tsx
@@ -23,8 +23,9 @@ export function LogCard({ log }: { log: Log }) {
           {log.location}
         </div>
         <div className="flex flex-col justify-between items-end gap-4">
-          <div className="font-bold text-xs uppercase text-neutral-500 dark:text-neutral-400 group-hover:text-neutral-600">
-            {log.timeOff}
+          <div className="font-bold text-[0.6rem] uppercase text-neutral-500 dark:text-neutral-400 group-hover:text-neutral-600">
+            <LightbulbOff className="h-4 w-4 mr-1 text-red-700 dark:text-[#ff5544] inline-flex" />
+            Time off: {log.timeOff}
           </div>
           <div className="text-neutral-500 dark:text-neutral-400 text-[0.6rem] tracking-tighter uppercase font-bold">
             {log.timeBackOn === "0" ? (
